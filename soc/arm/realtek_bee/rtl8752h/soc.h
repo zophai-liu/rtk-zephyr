3a20bda81115be359b448b3e6a690535bd84919c
Zophai Liu <zophai_liu@realsil.com.cn>
2024-09-05 19:09:08 +0800

    soc: arm: realtek_bee: add rtl8752h
    add basic files for rtl8752h
    
    Signed-off-by: Zophai Liu <zophai_liu@realsil.com.cn>

---
+++ soc/arm/realtek_bee/rtl8752h/soc.h
@@ -0,0 +1,10 @@
+#ifndef ZEPHYR_SOC_ARM_REALTEK_RTL8752H_SOC_H
+#define ZEPHYR_SOC_ARM_REALTEK_RTL8752H_SOC_H
+
+#ifndef _ASMLANGUAGE
+
+#include <rtl876x.h>
+
+#endif /* !_ASMLANGUAGE */
+
+#endif /* __SOC_H__ */
