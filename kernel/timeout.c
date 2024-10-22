50edaba5d46dbdfb7452c025ba9189d443ba7460
Zophai Liu <zophai_liu@realsil.com.cn>
2024-10-22 15:28:35 +0800

    kernel: timeout: workaround osif systick_handler
    This workaround will be removed in the future
    
    Signed-off-by: Zophai Liu <zophai_liu@realsil.com.cn>

--- kernel/timeout.c
+++ kernel/timeout.c
@@ -87,7 +87,7 @@ static int32_t next_timeout(void)
 	int32_t ret;
 
 	if ((to == NULL) ||
-	    ((int64_t)(to->dticks - ticks_elapsed) > (int64_t)INT_MAX)) {
+		((int64_t)(to->dticks - ticks_elapsed) > (int64_t)INT_MAX)) {
 		ret = MAX_WAIT;
 	} else {
 		ret = MAX(0, to->dticks - ticks_elapsed);
@@ -114,7 +114,7 @@ void z_add_timeout(struct _timeout *to, _timeout_func_t fn,
 		struct _timeout *t;
 
 		if (IS_ENABLED(CONFIG_TIMEOUT_64BIT) &&
-		    (Z_TICK_ABS(timeout.ticks) >= 0)) {
+			(Z_TICK_ABS(timeout.ticks) >= 0)) {
 			k_ticks_t ticks = Z_TICK_ABS(timeout.ticks) - curr_tick;
 
 			to->dticks = MAX(1, ticks);
@@ -228,8 +228,8 @@ void sys_clock_announce(int32_t ticks)
 	struct _timeout *t;
 
 	for (t = first();
-	     (t != NULL) && (t->dticks <= announce_remaining);
-	     t = first()) {
+		 (t != NULL) && (t->dticks <= announce_remaining);
+		 t = first()) {
 		int dt = t->dticks;
 
 		curr_tick += dt;
@@ -338,3 +338,61 @@ void z_vrfy_sys_clock_tick_set(uint64_t tick)
 	z_impl_sys_clock_tick_set(tick);
 }
 #endif /* CONFIG_ZTEST */
+
+#ifdef CONFIG_SOC_FAMILY
+/* To support RTK PM */
+struct _timeout *get_first_timeout(void)
+{
+	sys_dnode_t *t = sys_dlist_peek_head(&timeout_list);
+
+	return t == NULL ? NULL : CONTAINER_OF(t, struct _timeout, node);
+}
+
+struct _timeout *get_next_timeout(struct _timeout *t)
+{
+	sys_dnode_t *n = sys_dlist_peek_next(&timeout_list, &t->node);
+
+	return n == NULL ? NULL : CONTAINER_OF(n, struct _timeout, node);
+}
+
+void sys_clock_announce_only_add_ticks(int32_t ticks)
+{
+	k_spinlock_key_t key = k_spin_lock(&timeout_lock);
+
+	curr_tick += ticks;
+
+	struct _timeout *t;
+
+	for (t = first();
+		 (t != NULL) && (t->dticks <= ticks);
+		 t = next(t)) {
+		int dt = t->dticks;
+
+		t->dticks = 0;
+
+		ticks -= dt;
+	}
+
+	if (t != NULL) {
+		t->dticks -= ticks;
+	}
+
+	k_spin_unlock(&timeout_lock, key);
+}
+
+void sys_clock_announce_process_timeout(void)
+{
+	k_spinlock_key_t key = k_spin_lock(&timeout_lock);
+
+	struct _timeout *t;
+
+	for (t = first();
+		 (t != NULL) && (t->dticks == 0);
+		 t = first()) {
+		remove_timeout(t);
+		t->fn(t);
+	}
+
+	k_spin_unlock(&timeout_lock, key);
+}
+#endif
