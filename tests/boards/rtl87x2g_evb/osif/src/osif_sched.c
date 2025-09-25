56234c2e118e7eb740816c0ed529d0d67591fe0a
Zhiyuan Tang <zhiyuan_tang@realsil.com.cn>
2025-09-25 11:24:05 +0800

    tests: rtl87x2g: add osif testcase
    1. add osif testcases for rtl87x2g
    
    Signed-off-by: Zhiyuan Tang <zhiyuan_tang@realsil.com.cn>

---
+++ tests/boards/rtl87x2g_evb/osif/src/osif_sched.c
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2025 Realtek Semiconductor Corp.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <zephyr/ztest.h>
+#include <zephyr/kernel.h>
+#include <os_sched.h>
+#include <trace.h>
+
+#define WAIT_TIME_US 1000000
+
+/* Specify accepted tolerance. On some Zephyr platforms  (e.g. nRF5x) the busy
+ * wait loop and the system timer are based on different mechanisms and may not
+ * align perfectly. 1 percent base intolerance is to cover CPU processing in the
+ * test.
+ */
+#define TOLERANCE_PPC 1
+
+/**
+ * @brief Test os_sys_time_get
+ *
+ * @see os_sys_time_get_zephyr()
+ */
+ZTEST(osif_sched, test_os_sys_time_get)
+{
+	uint64_t start_time, stop_time, diff, max, min;
+
+	start_time = os_sys_time_get();
+	k_busy_wait(WAIT_TIME_US);
+	stop_time = os_sys_time_get();
+
+	diff = (uint64_t)(stop_time - start_time) * USEC_PER_MSEC;
+
+	max = WAIT_TIME_US + (TOLERANCE_PPC * WAIT_TIME_US / 100);
+	min = WAIT_TIME_US - (TOLERANCE_PPC * WAIT_TIME_US / 100);
+
+	zassert_true(diff <= max && diff >= min, "start %lld stop %lld (diff %lld) wait %d us\n",
+		     start_time, stop_time, diff, WAIT_TIME_US);
+}
+ZTEST_SUITE(osif_sched, NULL, NULL, NULL, NULL, NULL);
