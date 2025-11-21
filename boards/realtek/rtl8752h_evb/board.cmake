29b8cd6e03c740e91aeeb1bd3d50e314fd535b77
Zophai Liu <zophai_liu@realsil.com.cn>
2025-11-21 14:13:45 +0800

    rtl8752h: add support for pyocd runner
    pyocd can be a runner besides jlink
    
    Signed-off-by: Zophai Liu <zophai_liu@realsil.com.cn>

--- boards/realtek/rtl8752h_evb/board.cmake
+++ boards/realtek/rtl8752h_evb/board.cmake
@@ -1,5 +1,7 @@
 # Copyright (c) 2024 Realtek Semiconductor Corp.
 # SPDX-License-Identifier: Apache-2.0
 board_runner_args(jlink "--device=RTL8752H" "--speed=4000")
+board_runner_args(pyocd "--target=RTL8752H" "--tool-opt=--pack=${ZEPHYR_REALTEK_ZEPHYR_PROJECT_MODULE_DIR}/support/${CONFIG_SOC_SERIES}/Realtek.RTL8752H_DFP.1.0.0.pack")
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
