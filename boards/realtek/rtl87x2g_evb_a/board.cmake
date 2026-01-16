62df942605b796c58bf111b12bd8376165ced98f
Zhiyuan Tang <zhiyuan_tang@realsil.com.cn>
2026-01-16 16:46:45 +0800

    license: sycronize license
    Signed-off-by: Zhiyuan Tang <zhiyuan_tang@realsil.com.cn>

--- boards/realtek/rtl87x2g_evb_a/board.cmake
+++ boards/realtek/rtl87x2g_evb_a/board.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2026 Realtek Semiconductor Corp.
+# Copyright (c) 2026, Realtek Semiconductor Corporation
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(jlink "--device=RTL87X2G" "--speed=4000")
--- drivers/pinctrl/pinctrl_bee.c
+++ drivers/pinctrl/pinctrl_bee.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2026 Realtek Semiconductor Corp.
+ * Copyright (c) 2026, Realtek Semiconductor Corporation
  *
  * SPDX-License-Identifier: Apache-2.0
  */
