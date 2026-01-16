682496408f64a22823a2c2dd686d4f0a4e7a12fa
Zophai Liu <zophai_liu@realsil.com.cn>
2026-01-16 14:18:31 +0800

    west: update realtek bee mpcli runner name
    Update realtek bee mpcli runner name
    
    Signed-off-by: Zophai Liu <zophai_liu@realsil.com.cn>

--- boards/realtek/rtl8752h_evb/board.cmake
+++ boards/realtek/rtl8752h_evb/board.cmake
@@ -3,5 +3,5 @@
 
 board_runner_args(jlink "--device=RTL8752H" "--speed=4000")
 
-include(${ZEPHYR_BASE}/boards/common/bee.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/mpcli.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
